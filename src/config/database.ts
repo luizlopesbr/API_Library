5e818afa414c489b3a19a39e04cb5fab5c7e77fd
Luiz Lopes <luizlopesbr@gmail.com>
2025-04-14 21:32:19 -0300

    feat: cria o pool de conexão com o Postgres assim que o servidor entra em execução

---
+++ src/config/database.ts
@@ -0,0 +1,31 @@
+// db.ts ou database.ts
+import { Pool } from "pg"
+
+// Estendendo o escopo global
+declare global {
+    var connection: Pool | undefined
+}
+
+const pool = new Pool({
+    connectionString: process.env.CONNECTION_STRING,
+})
+
+async function connect() {
+    if (global.connection) {
+        return global.connection.connect()
+    }
+
+    const client = await pool.connect()
+    console.log("Criou o pool de conexão")
+
+    const res = await client.query("select now()")
+    console.log(res.rows[0])
+    client.release()
+
+    global.connection = pool
+
+    return pool.connect()
+}
+
+
+export { connect }
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
+import { connect } from "./config/database"
+
 
 const PORT:number = parseInt(`${process.env.PORT || 3000}`);
 
 import app from "./app";
 
+connect()
+    .then(() => {
+        app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}!`));
+    })
 
-
-app.listen(PORT, () => console.log(`Server is running at ${PORT}!`));
+    .catch((err) => {
+        console.error("Erro ao conectar ao banco de dados:", err)
+        process.exit(1) // Encerra a aplicação se não conseguir conectar
+      })
